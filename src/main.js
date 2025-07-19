6071593bc4ee6d5aae3827f387d6b90624f0d1ea
unknown <kdpavel8@gmail.com>
2025-07-19 11:09:25 +0500

    chore: Auto-register SFCs and modules in main.js

--- src/main.js
+++ src/main.js
@@ -2,4 +2,30 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// components and ui registration
+const vueFiles = import.meta.glob(
+    ['./components/**/*.vue', './ui/**/*.vue'],
+    { eager: true }
+)
+
+Object.entries(vueFiles).forEach(([path, module]) => {
+    const name = path
+        .split('/')
+        .pop()
+        .replace(/\.\w+$/, '')
+    app.component(name, module.default)
+})
+
+// modules registration
+const moduleIndexes = import.meta.glob('./modules/**/index.js', { eager: true })
+
+Object.values(moduleIndexes).forEach(mod => {
+    const map = mod.default || {}
+    Object.entries(map).forEach(([name, component]) => {
+        app.component(name, component)
+    })
+})
+
+app.mount('#app')
