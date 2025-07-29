c0347c7a4127e06943e49ecdc38bc16c33e51213
unknown <kdpavel8@gmail.com>
2025-07-30 01:03:42 +0500

    feat: upgrade database and API layer
    - Bump database version to 14
    - Add error handling in API methods
    - Implement new task hierarchy methods in API:
      * getTasksByProjectId
      * getSubtasksByTaskId
      * deleteTaskWithSubtasks
    - Update task creation with project and parent relations

--- src/modules/tasks/api/index.js
+++ src/modules/tasks/api/index.js
@@ -1,58 +1,84 @@
 import todoDB from "~/indexDB/todoDB/index.js"
 
-async function getTasks () {
-    try {
-        return await todoDB.tasks.getAll()
-    } catch (error) {
-        console.error(error)
-    }
+async function getTasks() {
+  try {
+    return await todoDB.tasks.getAllRootTasks()
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
-async function getTaskById (id) {
-    try {
-        if (!id) throw new Error("[API] task id not found")
+async function getTaskById(id) {
+  try {
+    if (!id) throw new Error("[API] task id is required")
 
-        return await todoDB.tasks.getById(id)
-    } catch (error) {
-        console.error(error)
-    }
+    return await todoDB.tasks.getTaskWithSubtasks(id)
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
-async function createTask (task) {
-    try {
-        return await todoDB.tasks.post(task)
-    } catch (error) {
-        console.error(error)
-    }
+async function getTasksByProjectId(projectId) {
+  try {
+    if (!projectId) throw new Error("[API] task is required")
+
+    return await todoDB.tasks.getTasksByProjectId(projectId)
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+async function getSubtasksByTaskId(projectId) {
+  try {
+    if (!projectId) throw new Error("[API] task is required")
+
+    return await todoDB.tasks.getSubtasksByTaskId(projectId)
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
-async function updateTask (id, task) {
-    try {
-        if (!id) throw Error("[API] task id not found")
-
-        return await todoDB.tasks.patch(id, task)
-    } catch (error) {
-        console.error(error)
-    }
+async function createTask(task) {
+  try {
+    return await todoDB.tasks.post(task)
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
-async function deleteTask (id) {
-    try {
-        if (!id) throw Error("[API] task id not found")
+async function updateTask(id, task) {
+  try {
+    if (!id) throw Error("[API] task id not found")
 
-        return await todoDB.tasks.del(id)
-    } catch (error) {
-        console.error(error)
-    }
+    return await todoDB.tasks.patch(id, task)
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
 }
 
+async function deleteTask(id) {
+  try {
+    if (!id) throw Error("[API] task id is required")
 
-async function getStatuses () {
+    return await todoDB.tasks.deleteTaskWithSubtasks(id)
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+async function getStatuses() {
   try {
     return await todoDB.statuses.getAll()
   } catch (error) {
     console.error(error)
+    throw error
   }
 }
 
-export default { getTasks, getTaskById, createTask, updateTask, deleteTask, getStatuses }
+export default { getTasks, getTaskById, getSubtasksByTaskId, createTask, updateTask, deleteTask, getStatuses, getTasksByProjectId }
